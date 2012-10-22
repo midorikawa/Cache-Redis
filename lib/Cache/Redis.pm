5a5ec89dd42386210b7b8f8db85e86cd9b6a5a0a
User Tooru midorikawa <tooru@ns.omakase.org>
2012-10-22 16:23:10 +0900

    0.64 a few fix

--- lib/Cache/Redis.pm
+++ lib/Cache/Redis.pm
@@ -1,18 +1,19 @@
 package Cache::Redis;
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.04';
+$VERSION = '0.064';
 use Carp qw(croak);
 use Redis;
-my ( $packer, $unpacker, $server, $encoding, $reconnect );
+my ( $packer, $unpacker, $server, $encoding, $sock, $reconnect );
 
 sub new {
     my ( $class, %params ) = @_;
 
     $server = $ENV{REDIS_SERVER}
       || ( $params{host} || '127.0.0.1' ) . ":" . ( $params{port} || 6379 );
-	$encoding = $params{encoding} || undef;
-	$reconnect = $params{reconnect} || 60;
+    $encoding  = $params{encoding} || undef;
+    $sock      = $params{sock};
+    $reconnect = defined $params{reconnect} ? $params{reconnect} : 60;
     if ( $params{serialize_methods} ) {
         if ( ref $params{serialize_methods} ne 'ARRAY' ) {
             croak "serialize_methods is coderef onry";
@@ -24,44 +25,55 @@ sub new {
         $unpacker = $params{serialize_methods}->[1];
         $packer   = $params{serialize_methods}->[0];
     }
-    my $self = {
-        prefix => $params{prefix} || 'session',
-        redis  => Redis->new( server => $server, encoding => $encoding, reconnect => $reconnect, ),
-        server => $server,
+    bless {
+        prefix  => $params{prefix}  || 'session',
+        redis   => _redis(),
+        server  => $server,
         expires => $params{expires} || undef,
         serialize_methods => $params{serialize_methods}
-    };
-
-    bless $self, $class;
+    }, $class;
 }
+
+sub _redis {
+    Redis->new(
+        $sock ? ( sock => $sock ) : ( server => $server ),
+        defined $reconnect ? ( reconnect => $reconnect ) : (),
+        encoding => $encoding
+    );
+}
+
 sub _exec {
-	my ($self, $cond, @args) = @_;
-
-	my $ret = eval {$self->{redis}->$cond(@args)};
-	if ($@){
-		$self->{redis} = Redis->new( server => $server, encoding => $encoding, reconnect => $reconnect );
-		$ret = $self->{redis}->$cond(@args);
-	}
-	if ($self->{expires} and ($cond eq 'get' or $cond eq 'set')){
-		$self->{redis}->expire($args[0], $self->{expires});
-	}
-	$ret;
+    my ( $self, $cond, $key, @args ) = @_;
+    $key or return;
+    unshift @args, $self->{prefix} . "_" . $key;
+    my $ret = eval { $self->{redis}->$cond(@args) };
+    if ($@) {
+        warn '****REDIS RECONNECTED**** : ', $@, @args;
+        $self->{redis} = _redis();
+        $ret = $self->{redis}->$cond(@args);
+    }
+    if ( $self->{expires} and ( $cond eq 'get' or $cond eq 'set' ) ) {
+        $self->{redis}->expire( $args[0], $self->{expires} );
+    }
+    $ret;
 }
+
 sub get {
     my ( $self, $key ) = @_;
-    my $ret = $self->_exec('get', $key);
-    $unpacker && $ret ? $unpacker->($ret) : $ret;
+    my $ret = $self->_exec( 'get', $key );
+        $unpacker && $ret ? $unpacker->($ret)
+      : defined $ret ? $ret 
+      :        ();
 }
 
 sub set {
     my ( $self, $key, $val ) = @_;
-    $self->_exec('set', $key, $packer && $val ? $packer->($val) : $val );
+    $self->_exec( 'set', $key, $packer && $val ? $packer->($val) : $val );
 }
-
 
 sub remove {
     my ( $self, $key ) = @_;
-    $self->_exec('del', $key);
+    $self->_exec( 'del', $key );
 }
 
 1;
@@ -77,7 +89,10 @@ Cache::Redis -
 
 use Data::MessagePack;
 my $cr = Cache::Redis->new(
-	expires => 86400,
+	sock => '/tmp/redis.sock',
+	reconnect => 60, # default to 60
+	prefix => 'session', # default to 'session'
+	expires => 86400, # default to undef
     serialize_methods => [
         sub { Data::MessagePack->pack( +shift ) },
         sub { Data::MessagePack->unpack( +shift ) }
